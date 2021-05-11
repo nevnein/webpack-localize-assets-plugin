192215d8959086966cb7c17d546a639c8b7e6209
Hiroki Osame <hiroki.osame@gmail.com>
2021-05-11 02:20:19 -0400

    built

---
+++ dist/utils.d.ts
@@ -0,0 +1,7 @@
+import { Webpack, Compilation, WP5 } from './types';
+export declare const sha256: (input: string) => string;
+export declare function findSubstringLocations(string: string, substring: string): number[];
+export declare const isWebpack5: (wp: Webpack) => boolean;
+export declare const isWebpack5Compilation: (compilation: Compilation) => compilation is WP5.Compilation;
+export declare const toConstantDependency: any;
+export declare const deleteAsset: (compilation: Compilation, assetName: string, newAssetNames: string[]) => void;
